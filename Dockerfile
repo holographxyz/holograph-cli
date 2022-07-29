187038bf1f6e0fcce8b9a8e8020dacf8c13d7836
kostas demiris <kostas@holograph.xyz>
2022-07-29 18:00:33 +0300

    build the new versatile image for holo-cli terminators

--- Dockerfile
+++ Dockerfile
@@ -18,15 +18,16 @@ RUN yarn build
 
 RUN npm install -location=global ../holo-cli
 
-# default value
 ENV CONFIG_FILE=a-super-config-file.json
-# default value
 ENV PASSWORD=a-super-secret-password
+
+ENV HOLO_CLI_MODE=TeRmInAtOr
+ENV HOLO_INDEXER_HOST=ThE_FuTuRe
 
 EXPOSE 6000
 
 RUN chmod 755 /holo-cli/entrypoint.sh
 # notice: The ENTRYPOINT specifies a command that will always be executed when the container starts.
-ENTRYPOINT ["/holo-cli/entrypoint.sh"]
+#ENTRYPOINT ["/holo-cli/entrypoint.sh"]
 # notice: The CMD specifies arguments that will be fed to the ENTRYPOINT
 # https://docs.docker.com/engine/reference/builder/#understand-how-cmd-and-entrypoint-interact
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,25 @@
 #
 holo config --fromFile $CONFIG_FILE
 
-#
-holo operator --mode auto --sync --healthCheck --unsafePassword $PASSWORD
+# notice: sample
+#./bin/dev operator   --mode auto --healthCheck --sync --unsafePassword [password]
+#./bin/dev propagator --mode auto --healthCheck --sync --unsafePassword [password]
+#./bin/dev indexer    --host=https://develop.cxipchain.xyz --healthCheck
 
+if [ $HOLO_CLI_MODE == "operator" ]
+then
+  holo $HOLO_CLI_MODE --mode auto --sync --healthCheck --unsafePassword $PASSWORD
+
+elif [ $HOLO_CLI_MODE == "propagator" ]
+then
+  holo $HOLO_CLI_MODE --mode auto --sync --healthCheck --unsafePassword $PASSWORD
+
+elif [ $HOLO_CLI_MODE == "indexer" ]
+then
+  holo $HOLO_CLI_MODE --host=$HOLO_INDEXER_HOST --healthCheck
+
+else
+  echo
+  echo "-> ERROR...Hey, Should I run as operator, propagator, indexer or at least TERMINATOR?"
+  echo
+fi
