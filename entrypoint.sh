19fe34ebc1bf9626b8bfcf88a88906400fffe040
kostas demiris <kostas@holograph.xyz>
2022-10-06 18:20:35 +0300

    test the operators in experimental

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
+echo "-- The env vars are below... --"
 # notice: set the env vars
 if [[ $ENABLE_DEBUG == 'true' ]]
 then
   export ENABLE_DEBUG='env DEBUG=\*'
-  echo ${ENABLE_DEBUG}
+  echo "ENABLE_DEBUG=${ENABLE_DEBUG}"
 else
   export ENABLE_DEBUG=""
+  echo "ENABLE_DEBUG=${ENABLE_DEBUG}"
 fi
 
 if [[ $ENABLE_SYNC == 'true' ]]
 then
   export ENABLE_SYNC="--sync"
-  echo ${ENABLE_SYNC}
+  echo "ENABLE_SYNC=${ENABLE_SYNC}"
 else
   export ENABLE_SYNC=""
+  echo "ENABLE_SYNC=${ENABLE_SYNC}"
 fi
 
 if [[ $HEALTHCHECK == 'true' ]]
 then
   export HEALTHCHECK="--healthCheck"
-  echo ${HEALTHCHECK}
+  echo "HEALTHCHECK=${HEALTHCHECK}"
 else
   export HEALTHCHECK=""
+  echo "HEALTHCHECK=${HEALTHCHECK}"
 fi
 
 # notice: configure
@@ -31,15 +35,16 @@ holo config --fromFile $CONFIG_FILE
 # notice: run the specified app
 if [[ $HOLO_CLI_CMD == "operator" ]]
 then
-  eval $ENABLE_DEBUG ABI_ENVIRONMENT=${ABI_ENVIRONMENT} holo ${HOLO_CLI_CMD} --networks ${NETWORK} --mode ${MODE} ${ENABLE_SYNC} ${HEALTHCHECK} --unsafePassword ${PASSWORD}
+  echo $(eval ${ENABLE_DEBUG} ABI_ENVIRONMENT=${ABI_ENVIRONMENT} holo ${HOLO_CLI_CMD} --networks ${NETWORK} --mode ${MODE} ${ENABLE_SYNC} ${HEALTHCHECK} --unsafePassword ${PASSWORD})
+  eval env DEBUG=\* ABI_ENVIRONMENT=${ABI_ENVIRONMENT} holo ${HOLO_CLI_CMD} --networks ${NETWORK} --mode ${MODE} ${ENABLE_SYNC} ${HEALTHCHECK} --unsafePassword ${PASSWORD}
 
 elif [[ $HOLO_CLI_CMD == "propagator" ]]
 then
-  eval env $ENABLE_DEBUG ABI_ENVIRONMENT=$ABI_ENVIRONMENT holo $HOLO_CLI_CMD --mode $MODE $ENABLE_SYNC $HEALTHCHECK --unsafePassword $PASSWORD
+  eval $ENABLE_DEBUG ABI_ENVIRONMENT=$ABI_ENVIRONMENT holo $HOLO_CLI_CMD --mode $MODE $ENABLE_SYNC $HEALTHCHECK --unsafePassword $PASSWORD
 
 elif [[ $HOLO_CLI_CMD == "indexer" ]]
 then
-  eval env $ENABLE_DEBUG ABI_ENVIRONMENT=$ABI_ENVIRONMENT holo $HOLO_CLI_CMD --networks $NETWORK --mode $MODE --host=$HOLO_INDEXER_HOST $HEALTHCHECK
+  eval $ENABLE_DEBUG ABI_ENVIRONMENT=$ABI_ENVIRONMENT holo $HOLO_CLI_CMD --networks $NETWORK --mode $MODE --host=$HOLO_INDEXER_HOST $HEALTHCHECK
 
 else
   echo
