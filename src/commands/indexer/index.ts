805aabf3fb3ab494ba9033f2d3c85e7771223e52
Alexander <alexanderattar@gmail.com>
2022-10-06 18:18:22 -0400

    Increase attempts for nft update db job

--- src/commands/indexer/index.ts
+++ src/commands/indexer/index.ts
@@ -355,7 +355,8 @@ export default class Indexer extends Command {
 
     if (receipt.status === 1) {
       this.networkMonitor.structuredLog(network, `Checking if a bridge request was made at tx: ${transaction.hash}`)
-      const operatorJobPayload = this.networkMonitor.decodePacketEvent(receipt) ?? this.networkMonitor.decodeLzPacketEvent(receipt)
+      const operatorJobPayload =
+        this.networkMonitor.decodePacketEvent(receipt) ?? this.networkMonitor.decodeLzPacketEvent(receipt)
       const operatorJobHash = operatorJobPayload === undefined ? undefined : ethers.utils.keccak256(operatorJobPayload)
       if (operatorJobHash === undefined) {
         this.networkMonitor.structuredLog(network, `Could not extract cross-chain packet for ${transaction.hash}`)
@@ -771,7 +772,7 @@ export default class Indexer extends Command {
     this.networkMonitor.structuredLog(network, `Sending minted nft job to DBJobManager ${contractAddress}`)
 
     const job: DBJob = {
-      attempts: 0,
+      attempts: 3,
       network,
       timestamp: await this.getBlockTimestamp(network, transaction.blockNumber!),
       query: `${this.BASE_URL}/v1/nfts/${contractAddress}/${tokenId}`,
