2a189d3c886a8e6a565d9869c56e8e017439ef13
Alexander <alexanderattar@gmail.com>
2022-10-19 01:56:13 -0400

    Add progress on bond command

--- src/commands/operator/bond.ts
+++ src/commands/operator/bond.ts
@@ -1,7 +1,7 @@
 import * as inquirer from 'inquirer'
 import * as fs from 'fs-extra'
 import {CliUx, Command, Flags} from '@oclif/core'
-import {ethers} from 'ethers'
+import {BigNumber, BigNumberish, ethers} from 'ethers'
 
 import {ConfigNetwork, ConfigNetworks, ensureConfigFileIsValid} from '../../utils/config'
 
@@ -9,12 +9,14 @@ import {networksFlag, FilterType, OperatorMode, BlockJob, NetworkMonitor} from '
 import {supportedNetworks} from '../../utils/networks'
 import {getEnvironment} from '../../utils/environment'
 import {HOLOGRAPH_ADDRESSES, HOLOGRAPH_OPERATOR_ADDRESSES} from '../../utils/contracts'
+import {formatEther, formatUnits} from 'ethers/lib/utils'
+import {PodBondAmounts} from '../../types/HolographOperator'
 
 /**
  * Start
  * Description: The primary command for operating jobs on the Holograph network.
  */
-export default class Start extends Command {
+export default class Bond extends Command {
   static description = 'Start an operator up into a pod'
   static examples = ['$ holo operator:start --network <string> --pod <number> --amount <number> --unsafePassword']
   static flags = {
@@ -39,7 +41,7 @@ export default class Start extends Command {
    * Command Entry Point
    */
   async run(): Promise<void> {
-    const {flags} = await this.parse(Start)
+    const {flags} = await this.parse(Bond)
 
     // Check the flags
     let network = flags.network
@@ -94,44 +96,91 @@ export default class Start extends Command {
     CliUx.ux.action.stop()
 
     const holographOperatorABI = await fs.readJson(`./src/abi/${environment}/HolographOperator.json`)
-    const holographOperator = new ethers.Contract(
+    const operator = new ethers.Contract(
       HOLOGRAPH_OPERATOR_ADDRESSES[environment],
       holographOperatorABI,
       destinationWallet,
     )
 
-    // console.log(holographOperator)
-
-    const totalPods = await holographOperator.getTotalPods()
+    const totalPods = await operator.getTotalPods()
     this.log(`Total Pods: ${totalPods}`)
 
-    //   if (!pod) {
-    //     const prompt: any = await inquirer.prompt([
-    //       {
-    //         name: 'pod',
-    //         message: 'Enter the pod number to join',
-    //         type: 'list',
-    //         choices: [1, 2, 3, 4],
-    //         default: undefined,
-    //       },
-    //     ])
-    //     pod = prompt.pod
-    //   }
+    if (!pod) {
+      const prompt: any = await inquirer.prompt([
+        {
+          name: 'pod',
+          message: 'Enter the pod number to join',
+          type: 'list',
+          choices: [...Array(totalPods.toNumber() + 1).keys()].slice(1),
+        },
+      ])
+      pod = prompt.pod
+      this.log(`Joining pod: ${pod}`)
+    }
 
-    //   this.log(`Joining pod: ${pod}`)
+    const podBoundAmounts: PodBondAmounts = await operator.getPodBondAmounts(pod)
+    this.log(
+      `Pod ${pod} has a base bond amount of ${formatEther(podBoundAmounts.base)} and currently requires ${formatEther(
+        podBoundAmounts.current,
+      )} to bond.`,
+    )
+    this.log(`Enter an amount greater or equal to: ${formatEther(podBoundAmounts.current)} to bond.`)
 
-    //   if (!amount) {
-    //     const prompt: any = await inquirer.prompt([
-    //       {
-    //         name: 'amount',
-    //         message: 'Enter the amount of tokens to deposit (Units in Ether)',
-    //         type: 'list',
-    //         default: undefined,
-    //       },
-    //     ])
-    //     amount = prompt.amount
-    //   }
+    if (!amount) {
+      const prompt: any = await inquirer.prompt([
+        {
+          name: 'amount',
+          message: `Enter the amount of tokens to deposit (Units in Ether)`,
+          type: 'number',
+          validate: async (input: number) => {
+            if (typeof input === 'number' && input > 0 && input > parseFloat(formatEther(podBoundAmounts.current))) {
+              return true
+            }
 
-    //   this.log(`Depositing ${amount} tokens`)
+            return 'Input is not a valid bond amount'
+          },
+        },
+      ])
+      amount = prompt.amount
+      this.log(`Depositing ${amount} tokens`)
+    }
+
+    this.log(
+      `Bonding from ${destinationWallet.address} to pod ${pod} on network ${destinationNetwork} for ${amount} tokens`,
+    )
+
+    CliUx.ux.action.start('Calculating gas amounts and prices')
+    let gasLimit
+    try {
+      gasLimit = await operator.estimateGas.bondUtilityToken(
+        destinationWallet.address,
+        BigNumber.from(amount).mul(BigNumber.from('10').pow(18)),
+        pod,
+      )
+    } catch (error: any) {
+      this.error(error.reason)
+    }
+
+    // const gasPriceBase = await destinationWallet!.provider.getGasPrice()
+    // const gasPrice = gasPriceBase.add(gasPriceBase.div(ethers.BigNumber.from('4'))) // gasPrice = gasPriceBase * 1.25
+
+    // CliUx.ux.action.stop()
+    // this.log(
+    //   'Transaction is estimated to cost a total of',
+    //   ethers.utils.formatUnits(gasLimit.mul(gasPrice), 'ether'),
+    //   'native gas tokens (in Ether)',
+    // )
+
+    // const blockchainPrompt: any = await inquirer.prompt([
+    //   {
+    //     name: 'shouldContinue',
+    //     message: 'Next steps submit the transaction, would you like to proceed?',
+    //     type: 'confirm',
+    //     default: true,
+    //   },
+    // ])
+    // if (!blockchainPrompt.shouldContinue) {
+    //   this.error('Dropping command, no blockchain transactions executed')
+    // }
   }
 }
