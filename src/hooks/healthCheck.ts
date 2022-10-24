8fd4c21dfdf09c4875a6c61db3634c40e9d000e6
Natalie Bravo <natalie.bravo@outlook.com>
2022-10-24 12:14:14 -0300

    removed unused code lines

--- src/hooks/healthCheck.ts
+++ src/hooks/healthCheck.ts
@@ -49,26 +49,3 @@ const healthCheckHook = async function (options: startHealthCheckServerProps) {
 }
 
 export default healthCheckHook
-
-// const healthCheckHook = async function (options: startHealthCheckServerProps) {
-//   const {networkMonitor, healthCheckPort} = options
-
-//   const host = '0.0.0.0'
-//   const port = healthCheckPort ? healthCheckPort : 6000
-
-//   const server = http.createServer(function (req: IncomingMessage, res: ServerResponse) {
-//     res.setHeader('Content-Type', 'application/json')
-//     if (req.url === '/healthcheck') {
-//       const providerStatus = networkMonitor.getProviderStatus()
-//       res.writeHead(200)
-//       res.end(JSON.stringify({status: 'alive', providerStatus}))
-//     } else {
-//       res.writeHead(200)
-//       res.end(JSON.stringify({hello: 'evil person'}))
-//     }
-//   })
-
-//   server.listen(port, host, () => {
-//     console.log(`Server is running on http://${host}:${port}`)
-//   })
-// }
