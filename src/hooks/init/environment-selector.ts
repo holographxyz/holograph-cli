37b1a460a9721c841df709dac9314a53eda6cb60
Natalie Bravo <natalie.bravo@outlook.com>
2022-11-15 17:30:13 -0300

    removed BaseCommand and changed default env to testnet

--- src/hooks/init/environment-selector.ts
+++ src/hooks/init/environment-selector.ts
@@ -10,14 +10,17 @@ enum Environment {
 const environmentSelectorHook: Hook<'init'> = async function ({id, argv}) {
   if (id !== 'config') {
     const indexOfEnv = argv.indexOf('--env')
-    process.env.HOLOGRAPH_ENVIRONMENT = Environment.MAINNET
+
+    process.env.HOLOGRAPH_ENVIRONMENT = Environment.TESTNET
+
     if (indexOfEnv !== -1) {
       const environment = argv[indexOfEnv + 1]
       argv.splice(indexOfEnv, 2)
+
       if ((Object.values(Environment) as string[]).includes(environment)) {
         process.env.HOLOGRAPH_ENVIRONMENT = environment
       } else {
-        this.log('WARNING: Environment not identified. Using "mainnet"...')
+        this.log('WARNING: Environment not identified. Using "testnet"...')
       }
     }
   }
