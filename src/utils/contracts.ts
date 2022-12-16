cb43580fb870e14c384ac42869e31875e97bbf5f
Alexander <alexanderattar@gmail.com>
2022-12-16 16:26:38 -0500

    Add latest testnet holograph address

--- src/utils/contracts.ts
+++ src/utils/contracts.ts
@@ -23,7 +23,7 @@ export const HOLOGRAPH_ADDRESSES: {[key in Environment]: string} = {
   [Environment.localhost]: '0xa3931469C1D058a98dde3b5AEc4dA002B6ca7446'.toLowerCase(),
   [Environment.experimental]: '0x199728d88a68856868f50FC259F01Bb4D2672Da9'.toLowerCase(),
   [Environment.develop]: '0x8dd0A4D129f03F1251574E545ad258dE26cD5e97'.toLowerCase(),
-  [Environment.testnet]: '0x9789dA8945527eF133eE78736b410CBb23b47A85'.toLowerCase(),
+  [Environment.testnet]: '0x6429b42da2a06aA1C46710509fC96E846F46181e'.toLowerCase(),
   [Environment.mainnet]: '0xE236E8E4283795E85b3021198Af69ddED9e132CD'.toLowerCase(),
 }
 
