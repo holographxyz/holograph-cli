84d7787531c2f261f0a7c3d502cb87bc85d1ae74
Alexander <alexanderattar@gmail.com>
2023-01-09 22:21:52 -0500

    Add the human readable logs back with the tx details

--- src/utils/network-monitor.ts
+++ src/utils/network-monitor.ts
@@ -1925,22 +1925,21 @@ export class NetworkMonitor {
             txHash = keccak256(signedTx)
           }
 
-          // TODO: Reenable after figuring out what is causing the BigNumber error in value
-          // const debugTx = {
-          //   to: populatedTx.to,
-          //   from: populatedTx.from,
-          //   nonce: populatedTx.nonce,
-          //   data: populatedTx.data,
-          //   value: BigNumber.from(populatedTx.value ?? 0).toNumber(),
-          //   gasLimit: BigNumber.from(populatedTx.gasLimit).toNumber(),
-          //   gasPrice: gasPricing.isEip1559 ? 0 : formatUnits(BigNumber.from(populatedTx.gasPrice), 'gwei'),
-          //   maxFeePerGas: gasPricing.isEip1559 ? formatUnits(BigNumber.from(populatedTx.maxFeePerGas), 'gwei') : 0,
-          //   maxPriorityFeePerGas: gasPricing.isEip1559
-          //     ? formatUnits(BigNumber.from(populatedTx.maxPriorityFeePerGas), 'gwei')
-          //     : 0,
-          // }
-          // this.structuredLog(network, 'Attempting to send transaction -> ' + JSON.stringify(debugTx), tags)
-          this.structuredLog(network, 'Attempting to send transaction -> ' + JSON.stringify(populatedTx), tags)
+          // Transform tx details to be human readable and log them
+          const txDetails = {
+            to: populatedTx.to,
+            from: populatedTx.from,
+            nonce: populatedTx.nonce,
+            data: populatedTx.data,
+            value: BigNumber.from(populatedTx.value ?? 0).toNumber(),
+            gasLimit: BigNumber.from(populatedTx.gasLimit ?? 0).toNumber(),
+            gasPrice: gasPricing.isEip1559 ? 0 : formatUnits(BigNumber.from(populatedTx.gasPrice ?? 0), 'gwei'),
+            maxFeePerGas: gasPricing.isEip1559 ? formatUnits(BigNumber.from(populatedTx.maxFeePerGas ?? 0), 'gwei') : 0,
+            maxPriorityFeePerGas: gasPricing.isEip1559
+              ? formatUnits(BigNumber.from(populatedTx.maxPriorityFeePerGas ?? 0), 'gwei')
+              : 0,
+          }
+          this.structuredLog(network, 'Attempting to send transaction -> ' + JSON.stringify(txDetails), tags)
           tx = await this.providers[network].sendTransaction(signedTx)
           if (tx === null) {
             counter++
