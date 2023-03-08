0390101bb7fe3db42fb45589deb7e9b9f963761b
Kostas Demiris <kostas@holograpgh.xyz>
2023-03-08 18:01:28 +0200

    added the  var in Dockerfile and entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,7 +44,7 @@ holograph config --fromFile $CONFIG_FILE
 # notice: run the specified app
 if [[ $HOLO_CLI_CMD == "operator" ]]
 then
-  eval $ENABLE_DEBUG holograph $HOLO_CLI_CMD --env $HOLOGRAPH_ENVIRONMENT --networks $NETWORK --mode $MODE $ENABLE_SYNC $HEALTHCHECK --unsafePassword $PASSWORD $ENABLE_UNSAFE
+  eval $ENABLE_DEBUG holograph $HOLO_CLI_CMD --env $HOLOGRAPH_ENVIRONMENT --networks $NETWORK --host=$HOLO_OPERATOR_HOST --mode $MODE $ENABLE_SYNC $HEALTHCHECK --unsafePassword $PASSWORD $ENABLE_UNSAFE
 
 elif [[ $HOLO_CLI_CMD == "indexer" ]]
 then
