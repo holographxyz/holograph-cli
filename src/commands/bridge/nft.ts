724a8b88155a2fdaa10bde56bb3e38c4ac2a4801
Alexander <alexanderattar@gmail.com>
2023-04-06 19:16:11 -0400

    Lint

--- src/commands/bridge/nft.ts
+++ src/commands/bridge/nft.ts
@@ -176,6 +176,8 @@ export default class BridgeNFT extends Command {
     CliUx.ux.action.start('Retrieving collection smart contract')
     const collectionABI = abis.HolographDropERC721ABI
     const collection = new Contract(collectionAddress, collectionABI, this.networkMonitor.providers[sourceNetwork])
+    console.log(`Collection address is ${collection.address}`)
+
     CliUx.ux.action.stop()
 
     this.log(`tokenId is ${tokenId}`)
