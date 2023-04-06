7d9107dc7d36f5e55d9aba7b1178579b98df1ad7
Alexander <alexanderattar@gmail.com>
2023-04-06 19:13:07 -0400

    Disable check that only works for legacy collections

--- src/commands/bridge/nft.ts
+++ src/commands/bridge/nft.ts
@@ -181,13 +181,13 @@ export default class BridgeNFT extends Command {
     this.log(`tokenId is ${tokenId}`)
     CliUx.ux.action.start('Checking if token ID exists on ' + sourceNetwork + ' network.')
 
-    // TODO: There seems to be a bug with the exists function. It throws an error
-    const tokenExists: boolean = await collection.exists(tokenId)
-    CliUx.ux.action.stop()
-    if (!tokenExists) {
-      this.log('Token does not exist.')
-      this.exit()
-    }
+    // TODO: This only works for legacy collections and not drops
+    // const tokenExists: boolean = await collection.exists(tokenId)
+    // CliUx.ux.action.stop()
+    // if (!tokenExists) {
+    //   this.log('Token does not exist.')
+    //   this.exit()
+    // }
 
     const data: BytesLike = generateInitCode(
       ['address', 'address', 'uint256'],
