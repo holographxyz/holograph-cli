80c4077ecd817f29c347b091fe5849d6476fb292
sogoiii <sogoiii@gmail.com>
2023-04-20 08:44:08 -0700

    fix network id for polygon, adding polygon gas

--- src/utils/network-monitor.ts
+++ src/utils/network-monitor.ts
@@ -2116,8 +2116,15 @@ export class NetworkMonitor {
 
       contract = contract.connect(this.wallets[network])
       if (gasPrice === undefined) {
+        this.structuredLog(network, `About to get gas price from internal gas price functions`, tags)
         gasPrice = this.gasPrices[network].gasPrice!
         gasPrice = gasPrice.add(gasPrice.div(TWO))
+      }
+
+      if(network === 'polygon') {
+        this.structuredLog(network,`Gas Price before = ${formatUnits(gasPrice, 'gwei')}`, tags)
+        const staticGasPrice = BigNumber.from('400017425011')
+        gasPrice = gasPrice.gt(staticGasPrice) ? gasPrice : staticGasPrice
       }
 
       this.structuredLog(network, `Gas price is ${formatUnits(gasPrice, 'gwei')} GWEI`, tags)
