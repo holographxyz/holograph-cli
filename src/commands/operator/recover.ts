88d5c01274c17f0cab5e11e5d4021e5f2fcf8b25
sogoiii <sogoiii@gmail.com>
2023-07-04 23:02:52 -0700

    code cleanup

--- src/commands/operator/recover.ts
+++ src/commands/operator/recover.ts
@@ -191,16 +191,13 @@ export default class Recover extends OperatorJobAwareCommand {
     }
 
     this.log(`Number of jobs to resolve: ${txArray.length}`)
+
     // Check balance for every unique network in txArray
     const uniqueNetworks = txArray
       .filter((value, index, self) => {
         return self.findIndex(v => v.sourceChainId === value.sourceChainId) === index
       })
       .map(item => item.sourceChainId)
-
-    console.log(uniqueNetworks)
-    console.log(userWallet)
-
     await this.networkMonitor.checkWalletBalances(userWallet.address, uniqueNetworks as number[])
 
     for (const element of txArray) {
@@ -226,10 +223,6 @@ export default class Recover extends OperatorJobAwareCommand {
 
     this.exit()
   }
-
-  // async checkWalletBalance(address, networks) {
-  //   address.getBalance()
-  // }
 
   /**
    * Process a transaction and attempt to either handle the bridge out or bridge in depending on the event emitted
